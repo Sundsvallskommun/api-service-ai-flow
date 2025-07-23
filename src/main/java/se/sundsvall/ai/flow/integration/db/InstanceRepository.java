d7646f184ef180ca65b473e4def3b502bcd72302
André Söderlund <92517165+AndreSoderlund@users.noreply.github.com>
2025-07-23 12:24:48 +0200

    Added InstanceEntity (#40)
    * Added instance entity, this will store properties for each municipalityÃ's instance of intric

---
+++ src/main/java/se/sundsvall/ai/flow/integration/db/InstanceRepository.java
@@ -0,0 +1,13 @@
+package se.sundsvall.ai.flow.integration.db;
+
+import java.util.Optional;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import se.sundsvall.ai.flow.integration.db.model.InstanceEntity;
+
+@Repository
+public interface InstanceRepository extends JpaRepository<InstanceEntity, String> {
+
+	Optional<InstanceEntity> findByMunicipalityId(final String municipalityId);
+
+}
