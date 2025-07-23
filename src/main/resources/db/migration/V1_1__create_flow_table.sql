8b9a079887bedc35c9a1ef39dbcd78abe72ff3e3
AndreSoderlund <andre.soderlund@b3.se>
2025-07-23 09:48:03 +0200

    Added instance entity, this will store properties for each municipalityÃ's instance of intric

--- src/main/resources/db/migration/V1_1__create_flow_table.sql
+++ src/main/resources/db/migration/V1_1__create_flow_table.sql
@@ -1,9 +1,16 @@
-CREATE TABLE IF NOT EXISTS `flow`(
-    `id` VARCHAR(255) NOT NULL,
-    `version` INTEGER NOT NULL,
+CREATE TABLE IF NOT EXISTS `flow`
+(
+    `id`      VARCHAR(255) NOT NULL,
+    `version` INTEGER      NOT NULL,
     `content` TEXT,
     PRIMARY KEY (`id`, `version`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+) ENGINE = InnoDB
+  DEFAULT CHARSET = utf8mb4;
 
-ALTER TABLE `flow` ADD INDEX `idx_flow_id`(`id`);
-ALTER TABLE `flow` ADD INDEX `idx_flow_id_and_version`(`id`, `version`);
+ALTER TABLE `flow`
+    ADD INDEX `idx_flow_id` (`id`);
+ALTER TABLE `flow`
+    ADD INDEX `idx_flow_id_and_version` (`id`, `version`);
+
+
+
