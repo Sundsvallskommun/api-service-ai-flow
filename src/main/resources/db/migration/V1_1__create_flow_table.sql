6ce78a46138d74816815b38b2702d0f8831d1811
AndreSoderlund <andre.soderlund@b3.se>
2025-07-23 11:26:47 +0200

    Reformatted the sql files

--- src/main/resources/db/migration/V1_1__create_flow_table.sql
+++ src/main/resources/db/migration/V1_1__create_flow_table.sql
@@ -1,16 +1,12 @@
-CREATE TABLE IF NOT EXISTS `flow`
-(
+CREATE TABLE IF NOT EXISTS `flow` (
     `id`      VARCHAR(255) NOT NULL,
     `version` INTEGER      NOT NULL,
     `content` TEXT,
     PRIMARY KEY (`id`, `version`)
-) ENGINE = InnoDB
-  DEFAULT CHARSET = utf8mb4;
+) ENGINE = InnoDB DEFAULT CHARSET = utf8mb4;
 
-ALTER TABLE `flow`
-    ADD INDEX `idx_flow_id` (`id`);
-ALTER TABLE `flow`
-    ADD INDEX `idx_flow_id_and_version` (`id`, `version`);
+ALTER TABLE `flow` ADD INDEX `idx_flow_id` (`id`);
+ALTER TABLE `flow` ADD INDEX `idx_flow_id_and_version` (`id`, `version`);
 
 
 
--- src/main/resources/db/migration/V1_2__add_name_and_description.sql
+++ src/main/resources/db/migration/V1_2__add_name_and_description.sql
@@ -1,7 +1,5 @@
-ALTER TABLE `flow`
-    MODIFY COLUMN `content` TEXT NOT NULL;
-ALTER TABLE `flow`
-    ADD COLUMN `name` varchar(255) NOT NULL;
-ALTER TABLE `flow`
-    ADD COLUMN `description` TEXT;
+ALTER TABLE `flow` MODIFY COLUMN `content` TEXT NOT NULL;
+ALTER TABLE `flow` ADD COLUMN `name` varchar(255) NOT NULL;
+ALTER TABLE `flow` ADD COLUMN `description` TEXT;
+
 
