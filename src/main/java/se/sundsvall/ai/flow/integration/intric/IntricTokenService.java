26cf3ab053982c423d25e55da2e78e06a845c9a8
André Söderlund <92517165+AndreSoderlund@users.noreply.github.com>
2025-08-18 13:57:51 +0200

    Added logging for creation stage of the feign & rest clients (#49)

--- src/main/java/se/sundsvall/ai/flow/integration/intric/IntricTokenService.java
+++ src/main/java/se/sundsvall/ai/flow/integration/intric/IntricTokenService.java
@@ -2,12 +2,15 @@ package se.sundsvall.ai.flow.integration.intric;
 
 import static java.time.Instant.now;
 import static java.util.Optional.ofNullable;
+import static se.sundsvall.dept44.util.LogUtils.sanitizeForLogging;
 
 import com.auth0.jwt.JWT;
 import generated.intric.ai.AccessToken;
 import java.time.Instant;
 import java.util.HashMap;
 import java.util.Map;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Component;
 import org.zalando.problem.Problem;
@@ -16,6 +19,8 @@ import se.sundsvall.ai.flow.integration.intric.configuration.IntricTokenConfigur
 
 @Component
 public class IntricTokenService {
+
+	private static final Logger LOG = LoggerFactory.getLogger(IntricTokenService.class);
 
 	private final IntricTokenConfiguration intricTokenConfiguration;
 
@@ -40,6 +45,7 @@ public class IntricTokenService {
 		var token = intricTokens.get(municipalityId);
 		// If we don't have a token at all, or if it's expired - get a new one
 		if (token == null || token.accessToken() == null || (token.expiresAt() != null && token.expiresAt().isBefore(now()))) {
+			LOG.info("No valid token found for municipality ID '{}', retrieving a new one.", sanitizeForLogging(municipalityId));
 			var tokenResponse = retrieveToken(municipalityId);
 
 			var accessToken = ofNullable(tokenResponse.getBody())
@@ -50,6 +56,7 @@ public class IntricTokenService {
 			var jwt = JWT.decode(accessToken);
 			var tokenExpiration = jwt.getExpiresAtAsInstant();
 
+			LOG.info("Retrieved new token for municipality ID '{}', accessToken '{}' expires at '{}'.", sanitizeForLogging(municipalityId), sanitizeForLogging(accessToken), sanitizeForLogging(String.valueOf(tokenExpiration)));
 			intricTokens.put(municipalityId, new Token(accessToken, tokenExpiration));
 		}
 
