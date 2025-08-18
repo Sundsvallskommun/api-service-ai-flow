d0ed92851f1eeff88928451995392a778b2c8f65
AndreSoderlund <andre.soderlund@b3.se>
2025-08-18 15:44:00 +0200

    Updated how the feign client is created, was having issues with the previous setup.

--- src/main/java/se/sundsvall/ai/flow/integration/intric/configuration/IntricClientFactory.java
+++ src/main/java/se/sundsvall/ai/flow/integration/intric/configuration/IntricClientFactory.java
@@ -1,14 +1,17 @@
 package se.sundsvall.ai.flow.integration.intric.configuration;
 
+import static java.util.concurrent.TimeUnit.SECONDS;
 import static org.springframework.http.HttpHeaders.AUTHORIZATION;
 
+import feign.Logger;
+import feign.Request;
 import org.springframework.cloud.openfeign.FeignClientBuilder;
 import org.springframework.context.ApplicationContext;
 import org.springframework.stereotype.Component;
 import se.sundsvall.ai.flow.integration.db.model.InstanceEntity;
 import se.sundsvall.ai.flow.integration.intric.IntricClient;
 import se.sundsvall.ai.flow.integration.intric.IntricTokenService;
-import se.sundsvall.dept44.configuration.feign.FeignMultiCustomizer;
+import se.sundsvall.dept44.configuration.feign.decoder.ProblemErrorDecoder;
 
 @Component
 public class IntricClientFactory {
@@ -26,12 +29,13 @@ public class IntricClientFactory {
 
 		return new FeignClientBuilder(applicationContext)
 			.forType(IntricClient.class, clientName)
-			.customize(test -> FeignMultiCustomizer.create()
-				.withRequestInterceptor(template -> template.header(AUTHORIZATION, "Bearer " + tokenService.getToken(instanceEntity.getMunicipalityId())))
-				.withRequestTimeoutsInSeconds(instanceEntity.getConnectTimeout(), instanceEntity.getReadTimeout())
-				.composeCustomizersToOne())
+			.customize(builder -> builder
+				.errorDecoder(new ProblemErrorDecoder(clientName))
+				.requestInterceptor(template -> template.header(AUTHORIZATION, "Bearer " + tokenService.getToken(instanceEntity.getMunicipalityId())))
+				.logLevel(Logger.Level.FULL)
+				.dismiss404()
+				.options(new Request.Options(instanceEntity.getConnectTimeout(), SECONDS, instanceEntity.getReadTimeout(), SECONDS, true)))
 			.url(instanceEntity.getBaseUrl())
 			.build();
 	}
-
 }
