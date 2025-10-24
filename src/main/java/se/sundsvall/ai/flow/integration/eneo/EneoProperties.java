608dde405ca6f5e354814df66107b2e645909f4b
Martin Hansson <martinhansson80@gmail.com>
2025-10-24 14:05:11 +0200

    hydran-17585: Removed unused base-url

--- src/main/java/se/sundsvall/ai/flow/integration/eneo/EneoProperties.java
+++ src/main/java/se/sundsvall/ai/flow/integration/eneo/EneoProperties.java
@@ -11,7 +11,6 @@ import org.springframework.validation.annotation.Validated;
 @Validated
 @ConfigurationProperties(prefix = "integration.eneo")
 public record EneoProperties(
-	@NotBlank String baseUrl,
 
 	@Valid @NotNull Oauth2 oauth2,
 
